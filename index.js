652eca588f928cee006e18b6be84bdfbc85b3e2c
Priyanshu <priyanshu210021@gmail.com>
2025-06-13 10:57:45 +0530

    port issue

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import chat from "./src/controllers/chat.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
+const PORT = process.env.PORT || 5000;
 
 
 app.post("/api/chat", chat );  
 
 app.listen(process.env.PORT, () => {
-  console.log(`NegiBot backend running on port ${process.env.PORT}`);
+  console.log(`NegiBot backend running on port ${PORT}`);
 });
